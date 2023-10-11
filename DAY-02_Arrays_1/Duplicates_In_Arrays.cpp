7010cb74b78de009c6841458903489abd4d6f936
Harsh-Pandey <harshcse7571@gmail.com>
2023-10-11 08:51:33 -0700

    Solved

---
+++ DAY-02_Arrays_1/Duplicates_In_Arrays.cpp
@@ -0,0 +1,11 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         map<int,bool>mp ;
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if(mp[nums[i]]) return nums[i];
+//             mp[nums[i]] = true ;
+//         }
+//         return -1 ;
+//     }
+// };
